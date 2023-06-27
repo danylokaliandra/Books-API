186d6e51a698ee0f4c9986602ffb4c84de5874b4
danylokaliandra <danylokaliandra@gmail.com>
2023-06-27 19:44:14 -0300

    updated index file in folders

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,11 @@
 const userController = require("./userController");
 const bookController = require("./bookController");
 const libraryController = require("./libraryController");
+const authController = require("./authController");
 
-module.exports = { userController, bookController, libraryController };
+module.exports = {
+  userController,
+  bookController,
+  libraryController,
+  authController,
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const userRouter = require("./userRoute");
 const bookRouter = require("./bookRoute");
 const libaryRouter = require("./libraryRoute");
+const authRouter = require("./authRoute");
 
-module.exports = { userRouter, bookRouter, libaryRouter };
+module.exports = { userRouter, bookRouter, libaryRouter, authRouter };
