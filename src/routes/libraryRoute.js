cd34d28b7782ed2e58bb046fef13e35ba44805ad
danylokaliandra <danylokaliandra@gmail.com>
2023-06-27 19:53:34 -0300

    updated library route

--- src/routes/libraryRoute.js
+++ src/routes/libraryRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { libraryController } = require("../controllers");
+const { authMiddleware } = require("../middlewares/authMiddleware");
 
 router.get("/", libraryController.findAllLibraries);
 router.get("/:id", libraryController.findOneLibrary);
-router.post("/create", libraryController.createLibrary);
-router.put("/update/:id", libraryController.updateLibrary);
-router.delete("/delete/:id", libraryController.deleteLibrary);
+router.post("/create", authMiddleware, libraryController.createLibrary);
+router.put("/update/:id", authMiddleware, libraryController.updateLibrary);
+router.delete("/delete/:id", authMiddleware, libraryController.deleteLibrary);
 
 module.exports = router;
